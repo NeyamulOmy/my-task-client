3f45efd28cdf9e71b0fcb554609286b378cccde7
NeyamulOmy <neyamul.omy@gmail.com>
2022-12-29 13:44:34 +0600

    add task form added

--- src/components/pages/AddTask.js
+++ src/components/pages/AddTask.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const AddTask = () => {
+    const { register, handleSubmit } = useForm();
+    const handleAddTask = (data) => {
+        const imageHostKey = '86d70bc72aebfc501add17b267d86940'
+        const formData = new FormData();
+        const image = data.image[0];
+        formData.append('image', image)
+        const url = `https://api.imgbb.com/1/upload?key=${imageHostKey}`
+        fetch(url, {
+            method: 'POST',
+            body: formData
+        })
+            .then(res => res.json())
+            .then(imgData => {
+                if (imgData.success) {
+                    console.log(imgData.data.url)
+                }
+            })
+    }
     return (
-        <div>
-            Add Task
+        <div className="w-1/2 my-10 mx-auto">
+            <form onSubmit={handleSubmit(handleAddTask)} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="title">
+                        Title
+                    </label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="title" type="text" {...register('title')} placeholder="Title" />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="image">
+                        Image
+                    </label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="image" type="file" placeholder="Image" accept='image/*' {...register('image')} />
+                </div>
+                <div className="mb-6">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="description">
+                        Description
+                    </label>
+                    <textarea className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="description" type="text" placeholder="Description" {...register('description')} />
+
+                </div>
+                <div className="flex items-center justify-between">
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                        Add Task
+                    </button>
+
+                </div>
+            </form>
+
         </div>
     );
 };
